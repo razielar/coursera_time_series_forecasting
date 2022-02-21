d4cc732e0e8208f50be5a10424b3bb236f0321c5
razielar <razielar@gmail.com>
2022-02-21 16:07:42 +0100

    common nonstationarity to stationarity transformations

--- coursera_time_series/week_2/src/timeseriesFunctions.py
+++ coursera_time_series/week_2/src/timeseriesFunctions.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_time_series(x,y, title= "Title", xlabel= "time", ylabel= "series"):
     plt.plot(x,y,'k-')
@@ -6,3 +7,10 @@ def plot_time_series(x,y, title= "Title", xlabel= "time", ylabel= "series"):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(alpha= 0.3)
+
+def chunks_statistics(np_chunk_array):
+    print("{} | {:7} | {}".format("Chunk", "Mean", "Variance"))
+    print("-" * 26)
+    for i,j in enumerate(np_chunk_array, 1):
+        print("{:5} | {:.6} | {:.6}".format(i, np.mean(j), np.var(j)))
+
