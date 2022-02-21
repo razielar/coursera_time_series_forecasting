ef67b3841e7acb59ef80f294afa9036d0a0467ae
razielar <razielar@gmail.com>
2022-02-21 14:07:42 +0100

    time-series functions and start identifying stationarity

---
+++ coursera_time_series/week_2/src/timeseriesFunctions.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+def plot_time_series(x,y, title= "Title", xlabel= "time", ylabel= "series"):
+    plt.plot(x,y,'k-')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(alpha= 0.3)
