0f5abc0f1803d1144117713a1560f135ea847c0b
razielar <razielar@gmail.com>
2022-02-28 19:47:56 +0100

    mape, cv, and gridcv functions; start exercise

--- coursera_time_series/week_3/2_arima_sarima_models/src/timeseriesFunctions.py
+++ coursera_time_series/week_3/2_arima_sarima_models/src/timeseriesFunctions.py
@@ -12,6 +12,7 @@ def plot_time_series(x,y, title= "Title", xlabel= "time", ylabel= "series"):
     plt.grid(alpha= 0.3)
 
 def chunks_statistics(np_chunk_array):
+    # np.split
     print("{} | {:7} | {}".format("Chunk", "Mean", "Variance"))
     print("-" * 26)
     for i,j in enumerate(np_chunk_array, 1):
@@ -57,18 +58,59 @@ def future_preds_df(model,series,num_months):
     vals = model.predict(n_periods = num_months)
     return pd.DataFrame(vals,index = date_range_index)
 
-# def MAPE(predictions, actuals):
-#     """
-#     Implements Mean Absolute Percent Error (MAPE).
-#     Args:
-#         predictions (array like): a vector of predicted values.
-#         actuals (array like): a vector of actual values.
+def mape(df_cv):
+    """
+    MAPE: mean absolute percentage error
+    """
+    # return abs(df_cv.actual - df_cv.forecast).sum() / df_cv.actual.sum()
+    return abs((df_cv.actual - df_cv.forecast)/df_cv.actual).sum() / len(df_cv)
 
-#     Returns:
-#         numpy.float: MAPE value
-#     """
-#     if not (isinstance(actuals, pd.Series) and isinstance(predictions, pd.Series)):
-#         predictions, actuals = pd.Series(predictions), pd.Series(actuals)
-#     return ((predictions - actuals).abs() / actuals).mean()
+def cross_validate(series, horizon, start, step_size, order= (0,0,0), seasonal_order= (0,0,0,0), trend= None):
+    '''
+    Function to determine in and out of samples for testing of ARIMA model
+    ------
+    arguments:
+        series (series): time series input
+        horizon (int): how far in advance forecast is needed
+        start (int): starting location in series
+        step_size (int): how often to recalculate forecast
+        order (tuple): (p,d,q) order of the model
+        seasonal_order (tuple): (P,D,Q,s) seasonal order of model
+    Return
+    ------
+    Dataframe: gives forecast and actuals with date of prediction
+    '''
+    fcst= []
+    actual= []
+    date= []
+    for i in range(start, len(series)- horizon, step_size):
+        # print(i)
+        # print(series[i])
+        # print(series[:i+1])
+        model= SARIMAX(series[:i+1], order= order, seasonal_order= seasonal_order, trend= trend).fit()
+        fcst.append(model.forecast(steps= horizon)[-1])
+        actual.append(series[i+horizon])
+        date.append(series.index[i+horizon])
+    return pd.DataFrame({'forecast': fcst, 'actual': actual}, index= date)
 
-
+def grid_search_ARIMA(series, horizon, start, step_size, orders= [(1,0,0)], seasonal_orders= [(0,0,0,0)], trends= [None]):
+    best_mape = np.inf
+    best_order = None
+    best_seasonal_order = None
+    best_trend = None
+    for order_ in orders:
+        for seasonal_order_ in seasonal_orders:
+            for trend_ in trends:
+                cv = cross_validate(series,
+                                    horizon,
+                                    start,
+                                    step_size,
+                                    order = order_,
+                                    seasonal_order = seasonal_order_,
+                                    trend=trend_)
+                if mape(cv)<best_mape:
+                    best_mape = mape(cv)
+                    best_order = order_
+                    best_seasonal_order = seasonal_order_
+                    best_trend = trend_
+    return (best_order,best_seasonal_order, best_trend, np.round(best_mape, 6))
