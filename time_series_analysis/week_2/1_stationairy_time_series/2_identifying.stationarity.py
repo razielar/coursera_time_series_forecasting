8e680acb26fd0ca5c9e014f1475e4b5fb5068c1a
razielar <razielar@gmail.com>
2022-03-04 10:53:14 +0100

    week2-1-2

--- time_series_analysis/week_2/1_stationairy_time_series/2_identifying.stationarity.py
+++ time_series_analysis/week_2/1_stationairy_time_series/2_identifying.stationarity.py
@@ -4,7 +4,7 @@
 # # Identifying Stationarity
 # Feb 21st 2022
 
-# In[1]:
+# In[4]:
 
 
 import sys
@@ -14,19 +14,18 @@ import pandas as pd
 import os
 print(os.getcwd())
 import matplotlib.pyplot as plt
-# %matplotlib inline
 import seaborn as sns
 from IPython.display import display
 from src.timeseriesFunctions import plot_time_series
 from statsmodels.tsa.stattools import adfuller # Augmented Dickey-Fuller test
-# Config:
 SEED= 42
-pd.options.display.float_format = '{:,.2f}'.format
-sns.set_context("paper", font_scale= 1.5)
-plt.rcParams['axes.spines.right']= False
-plt.rcParams['axes.spines.top']= False
-plotsize = (13, 5)
-plt.rcParams['figure.figsize']= plotsize
+
+
+# In[3]:
+
+
+# Custom functions
+import src.colorsetup
 
 
 # ## Identifying Stationarity
@@ -34,7 +33,7 @@ plt.rcParams['figure.figsize']= plotsize
 # * 2) Summary statistics for mean and variance
 # * 3) Agumented Dickey-Fuller test
 
-# In[2]:
+# In[5]:
 
 
 np.random.seed(SEED)
@@ -47,7 +46,7 @@ trend= (time * 2.75) + stationary
 
 # ### 1) EDA of the time-series
 
-# In[3]:
+# In[6]:
 
 
 # 1) EDA of the time-series
@@ -56,7 +55,7 @@ plot_time_series(time, trend, title= "Non-stationarity Time-series w/Trend")
 
 # ### 2) Summary Statistics & Plots
 
-# In[4]:
+# In[7]:
 
 
 # split data into 10 chunks
@@ -64,7 +63,7 @@ chunks= np.split(trend, indices_or_sections= 10)
 chunks[0]
 
 
-# In[5]:
+# In[8]:
 
 
 print("{} | {:7} | {}".format("Chunk", "Mean", "Variance"))
@@ -73,7 +72,7 @@ for i,j in enumerate(chunks, 1):
     print("{:5} | {:.6} | {:.6}".format(i, np.mean(j), np.var(j)))
 
 
-# In[6]:
+# In[9]:
 
 
 def chunks_statistics(np_chunk_array):
@@ -83,14 +82,14 @@ def chunks_statistics(np_chunk_array):
         print("{:5} | {:.6} | {:.6}".format(i, np.mean(j), np.var(j)))
 
 
-# In[7]:
+# In[10]:
 
 
 pd.Series(trend).hist()
 plt.show()
 
 
-# In[8]:
+# In[11]:
 
 
 pd.Series(stationary).hist()
@@ -101,7 +100,7 @@ plt.show()
 # Null hypothesis= time-series is non-stationary  
 # Whether or not there's a high correlation between the current value and some lag values. That will be the case if: trends
 
-# In[9]:
+# In[12]:
 
 
 adf, pvalue, usedlag, nobs, critical_values, icbest = adfuller(trend)
@@ -111,7 +110,7 @@ print("Test statistics: {:.6}, P.value: {:.6}".format(adf, pvalue))
 
 # ## Exercise
 
-# In[10]:
+# In[13]:
 
 
 mytime= np.arange(100)
@@ -120,19 +119,19 @@ dataset_1= np.load(data_path + "dataset_SNS_1.npy")
 dataset_2= np.load(data_path + "dataset_SNS_2.npy")
 
 
-# In[11]:
+# In[14]:
 
 
 plot_time_series(mytime, dataset_1, title= "Dataset 1")
 
 
-# In[12]:
+# In[15]:
 
 
 plot_time_series(mytime, dataset_2, title= "Dataset 2")
 
 
-# In[13]:
+# In[16]:
 
 
 chunks_1= np.split(dataset_1, indices_or_sections= 10)
@@ -145,7 +144,7 @@ print('Dataset 2')
 chunks_statistics(chunk_2)
 
 
-# In[14]:
+# In[17]:
 
 
 series_1= pd.Series(dataset_1)
@@ -161,7 +160,7 @@ axes[1].set_title("Dataset 2")
 plt.show()
 
 
-# In[15]:
+# In[18]:
 
 
 adf_1, pvalue_1, usedlag_, nobs_, critical_values_1, icbest_ = adfuller(dataset_1)
@@ -179,10 +178,11 @@ print("p-value:", np.round(pvalue_2,4))
 print("crit values: ", critical_values_2)
 
 
-# In[ ]:
+# In[19]:
 
 
-
+import session_info
+session_info.show()
 
 
 # In[ ]:
